a196f9e517e4e42b5cef0e077ff18af8a9950a6f
gyahury <rudgns1243@naver.com>
2023-04-12 23:19:36 +0900

    [FRON-002] Style : title 및 로고 이미지 변경

--- Frontend/nuxt.config.js
+++ Frontend/nuxt.config.js
@@ -3,8 +3,8 @@ import colors from 'vuetify/es5/util/colors'
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - Frontend',
-    title: 'Frontend',
+    titleTemplate: 'CTD - AI Coach To Do', //3D3C3C - 80807C
+    title: 'Coach To Do',
     htmlAttrs: {
       lang: 'en'
     },
