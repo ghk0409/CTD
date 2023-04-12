e14ad15b156081037b4b3786c241891a8d4b98f1
gyahury <rudgns1243@naver.com>
2023-04-12 18:02:40 +0900

    [FRON-002] Style : 기본 레이아웃 설정 및 로고 추가

---
+++ Frontend/components/CtdLoGo.vue
@@ -0,0 +1,11 @@
+<template>
+  <img class="ctd-logo" alt="ctd-logo" src="/ctd-logo.png" />
+</template>
+
+<style>
+.ctd-logo {
+  margin-top: 20px;
+  margin-left: 18px;
+  height: 25px;
+}
+</style>
