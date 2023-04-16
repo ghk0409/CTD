d22b9d457028191e79d41fcf83138174e77d7c22
gyahury <rudgns1243@naver.com>
2023-04-16 13:39:06 +0900

    [FRON-002]Feat: 레이아웃 추가 및 라우터 설정 - 로그인 - 회원가입 - ai advice

---
+++ Frontend/pages/signup.vue
@@ -0,0 +1,33 @@
+<template>
+    <v-sheet width="300" class="mx-auto mt-7">
+        <v-form fast-fail @submit.prevent>
+            <v-text-field v-model="email" label="new email" :rules="emailRules"></v-text-field>
+            <v-text-field v-model="password" label="password" type="password" :rules="passwordRules"></v-text-field>
+            <v-text-field v-model="password" label="confirm password" type="password" :rules="passwordRules"></v-text-field>
+            <v-btn type="submit" block class="mt-2">submit</v-btn>
+        </v-form>
+    </v-sheet>
+</template>
+
+<script>
+export default {
+    data: () => ({
+        email: '',
+        emailRules: [
+            value => {
+                if (value?.length > 3) return true
+
+                return 'email must be at least 3 characters.'
+            },
+        ],
+        password: '',
+        passwordRules: [
+            value => {
+                if (/[^0-9]/.test(value)) return true
+                
+                return 'password can not contain digits.'
+            },
+        ],
+    }),
+}
+</script>
