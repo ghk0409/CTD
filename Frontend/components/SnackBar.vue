9504c3e99903acb210c140440a4f30233389f786
gyahury <rudgns1243@naver.com>
2023-04-17 16:48:52 +0900

    [FRON-003] Feat: 전역 알림 메시지 설정 추가

---
+++ Frontend/components/SnackBar.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="text-center ma-2">
+        <v-snackbar v-model="snackbar">
+            {{ snackbarMessage }}
+            <template v-slot:action="{ attrs }">
+                <v-btn :color="btnColor" text v-bind="attrs" @click="snackbar = false">
+                    Close
+                </v-btn>
+            </template>
+        </v-snackbar>
+    </div>
+</template>
+  
+<script>
+export default {
+    data: () => ({
+        snackbar: false,
+        btnColor: "pink",
+        snackbarMessage: "",
+    }),
+    methods: {
+        showMessage(msg, color,duration = 3000) {
+            this.snackbarMessage = msg;
+            this.btnColor = color;
+            this.snackbar = true;
+            setTimeout(() => {
+                this.snackbar = false;
+            }, duration);
+        },
+    },
+};
+</script>
--- Frontend/pages/signup.vue
+++ Frontend/pages/signup.vue
@@ -1,42 +1,41 @@
 <template>
-    <v-sheet width="300" class="mx-auto mt-7">
-        <v-form fast-fail ref="form" @submit.prevent="submitForm">
-            <v-text-field v-model="email" label="new email" :rules="emailRules" required></v-text-field>
-            <v-text-field v-model="password" label="password" type="password" :rules="passwordRules"
-                required></v-text-field>
-            <v-text-field v-model="confirmPassword" label="confirm password" type="password" :rules="confirmPasswordRules"
-                required></v-text-field>
-            <v-btn type="submit" block class="mt-2">submit</v-btn>
-        </v-form>
-    </v-sheet>
+  <v-sheet width="300" class="mx-auto mt-7">
+    <v-form fast-fail ref="form" @submit.prevent="submitForm">
+      <v-text-field v-model="email" label="new email" :rules="emailRules" required></v-text-field>
+      <v-text-field v-model="password" label="password" type="password" :rules="passwordRules" required></v-text-field>
+      <v-text-field v-model="confirmPassword" label="confirm password" type="password" :rules="confirmPasswordRules"
+        required></v-text-field>
+      <v-btn type="submit" block class="mt-5">submit</v-btn>
+    </v-form>
+  </v-sheet>
 </template>
 
 <script>
 export default {
-    data: () => ({
-        email: '',
-        emailRules: [
-            value => !!value || '이메일을 입력해주세요.',
-            value => /.+@.+/.test(value) || '이메일이 올바른 형식이 아닙니다.'
-        ],
-        password: '',
-        passwordRules: [
-            value => !!value || '비밀번호를 입력해주세요.',
-            value => (value && value.length >= 8) || '비밀번호는 8자 이상으로 구성되어야 합니다.',
-            value => /[A-Z]/.test(value) || '비밀번호는 대문자가 포함되어야 합니다.',
-            value => /[!@#$%^&*(),.?":{}|<>]/.test(value) || '비밀번호는 특수문자가 포함되어야 합니다.'
-        ],
-        confirmPassword: '',
-    }),
-    computed: {
-        confirmPasswordRules() {
-            return [
-                value => !!value || '비밀번호를 입력해주세요.',
-                value => value === this.password || '비밀번호가 일치하지 않습니다.'
-            ];
-        },
+  data: () => ({
+    email: '',
+    emailRules: [
+      value => !!value || '이메일을 입력해주세요.',
+      value => /.+@.+/.test(value) || '이메일이 올바른 형식이 아닙니다.'
+    ],
+    password: '',
+    passwordRules: [
+      value => !!value || '비밀번호를 입력해주세요.',
+      value => (value && value.length >= 8) || '비밀번호는 8자 이상으로 구성되어야 합니다.',
+      value => /[A-Z]/.test(value) || '비밀번호는 대문자가 포함되어야 합니다.',
+      value => /[!@#$%^&*(),.?":{}|<>]/.test(value) || '비밀번호는 특수문자가 포함되어야 합니다.'
+    ],
+    confirmPassword: '',
+  }),
+  computed: {
+    confirmPasswordRules() {
+      return [
+        value => !!value || '비밀번호를 입력해주세요.',
+        value => value === this.password || '비밀번호가 일치하지 않습니다.'
+      ];
     },
-    methods: {
+  },
+  methods: {
     async submitForm() {
       if (this.$refs.form.validate()) {
         try {
@@ -44,11 +43,16 @@ export default {
             email: this.email,
             password: this.password,
           });
-          //완료 후 처리 -> 스낵바 추가 예정
+          this.$root.$emit('showSnackbar', '회원가입이 성공적으로 완료되었습니다.', 'blue', 5000);
           this.$router.push('/');
         } catch (error) {
-          //실패 후 처리 -> 스낵바 추가 예정
           console.error('Signup failed:', error);
+          if (error.response) {
+            this.$root.$emit('showSnackbar', '회원가입에 실패했습니다. 다시 시도해주세요.', 'red', 5000);
+          } else {
+            this.$root.$emit('showSnackbar', '네트워크 에러가 발생했습니다. 다시 시도해주세요.', 'red', 5000);
+          }
+          this.$router.push('/');
         }
       }
     },
