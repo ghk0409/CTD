ef707f0423e52e1c4fa62c6861816bbdd3cb69da
gyahury <rudgns1243@naver.com>
2023-04-23 00:22:43 +0900

    Feature: AI 코치 기능 정규식 적용 중

--- Frontend/pages/coaching.vue
+++ Frontend/pages/coaching.vue
@@ -4,7 +4,13 @@
             <span class="text-h5">AI Advice for you</span>
         </v-card-title>
         <v-card-text>
-            {{ postContent }}
+            <template v-if="coachingContent">
+                {{ coachingContent }}
+                <!-- <div v-html="formattedcoachingContent"></div> -->
+            </template>
+            <template v-else>
+                시간이 10초 정도 소요됩니다. 잠시만 기다려주세요.
+            </template>
         </v-card-text>
     </v-card>
 </template>
@@ -15,13 +21,19 @@ export default {
     middleware: 'auth',
     data() {
         return {
-            postContent: '',
+            coachingContent: '',
         };
+    },
+    computed: {
+        formattedcoachingContent() {
+            return this.coachingContent.replace(/(\d+)\.\s/g, '$&<br>');
+            
+        },
     },
     async mounted() {
         try {
             const response = await this.$axios.$post('/ai/claude');
-            this.postContent = response.data.claude;
+            this.coachingContent = response.data.claude;
         } catch (error) {
             console.error(error);
         }
