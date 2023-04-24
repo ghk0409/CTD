702922fd3e08670396d0fb732cfe028a26c4cf9c
JIHONG KIM <64270116+ghk0409@users.noreply.github.com>
2023-04-24 22:47:01 +0900

    Fix: ToDo 데이터 변경 시, Claude 데이터 삭제로 변경

--- Backend/src/todos/todos.service.ts
+++ Backend/src/todos/todos.service.ts
@@ -10,6 +10,7 @@ import { UserEntity } from 'src/users/entities/user.entity';
 import { EditTodoRequestDto, EditTodoResponseDto } from './dtos/edit-todo.dto';
 import { DeleteTodoResponseDto } from './dtos/delete-todo.dto';
 import { ListTodoResponseDto } from './dtos/list-todo.dto';
+import { AiEntity } from 'src/ai/entities/ai.entity';
 
 @Injectable()
 export class TodosService {
@@ -18,7 +19,15 @@ export class TodosService {
         private readonly todos: Repository<TodoEntity>,
         @InjectRepository(UserEntity)
         private readonly users: Repository<UserEntity>,
+        @InjectRepository(AiEntity)
+        private readonly aiRepository: Repository<AiEntity>,
     ) {}
+
+    // 해당 유저 ID의 Claude 데이터 삭제
+    private async removeClaude(userId: number): Promise<void> {
+        // softDelete를 사용하면 논리적 삭제(deleteAt 컬럼 활용), delete를 사용하면 물리적 삭제(완전 제거)
+        await this.aiRepository.delete({ user: { id: userId } });
+    }
 
     // 유저 To-Do 조회
     async getTodos(userId: number): Promise<ListTodoResponseDto> {
@@ -67,6 +76,9 @@ export class TodosService {
                 this.todos.create({ user, content, feel }),
             );
 
+            // 해당 유저의 Claude 데이터 삭제
+            await this.removeClaude(userId);
+
             return {
                 ok: true,
                 todoId: todo.id,
@@ -101,6 +113,10 @@ export class TodosService {
                 ...todo,
                 ...editTodoRequestDto,
             });
+
+            // 해당 유저의 Claude 데이터 삭제
+            await this.removeClaude(userId);
+
             return {
                 ok: true,
             };
@@ -128,6 +144,10 @@ export class TodosService {
             }
 
             await this.todos.remove(todo);
+
+            // 해당 유저의 Claude 데이터 삭제
+            await this.removeClaude(userId);
+
             return {
                 ok: true,
             };
